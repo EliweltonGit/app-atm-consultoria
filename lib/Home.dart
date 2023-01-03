b44c5d67d71050ef2420f9ce7184688180549472
Eiwelton18 <eliwelton.g.p@gmail.com>
2023-01-03 09:36:34 -0300

    Intarface Inicial

---
+++ lib/Home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("ATM Consultoria"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+      ),
+    );
+  }
+}
